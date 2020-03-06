0e8b18c307c7494e25357ae6a35aedc6753237df
Yusheng Zhou 周宇盛 <jasonzhouu@outlook.com>
2020-03-06 13:02:41 +0800

    -

--- server/Notes.js
+++ server/Notes.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 function Notes() {
     // @todo: 如果notes规模太大，会有性能问题。后续改为 Redis 数据库。
     const jsonPath = './test/notes.json'
+
+    // @todo: 如果读取失败，或者为空，则初始化为 []
     let rawData = fs.readFileSync(jsonPath);
     let notes = JSON.parse(rawData)
 
