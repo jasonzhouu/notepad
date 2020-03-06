e21e856df1e82747dcc161b7b3c68f6b4d2e5754
Yusheng Zhou 周宇盛 <jasonzhouu@outlook.com>
2020-03-06 18:58:15 +0800

    事件格式化

--- client/index.js
+++ client/index.js
@@ -117,16 +117,38 @@ function Notes() {
     }
     function parseDate(date) {
         let dateObject = new Date(date)
+
+        let year = dateObject.getFullYear()
+        let month = dateObject.getMonth()
+        let day = dateObject.getDate()
+
+        let now = new Date()
+        let nowYear = now.getFullYear()
+        let nowMonth = now.getMonth()
+        let nowDay = now.getDate()
+
         let dateFormat = ''
-        dateFormat += dateObject.getFullYear()
-        dateFormat += '/'
-        dateFormat += dateObject.getMonth()
-        dateFormat += '/'
-        dateFormat += dateObject.getDate()
-        dateFormat += ' '
-        dateFormat += dateObject.getHours()
-        dateFormat += ':'
-        dateFormat += dateObject.getMinutes()
+        if(year != nowYear) {
+            // 不是今年发的，显示年月日
+            dateFormat = `${year}/${month}/${day}`
+        } else if(month != nowMonth  || day != nowDay) {
+            // 不是今天发的，只显示月、日
+            dateFormat = `${month}/${day}`
+        } else if( year == nowYear && month == nowMonth && day == nowDay) {
+            // 今天发的，只显示多久之前
+            let interval = now - dateObject
+            let second = (interval/1000).toFixed(0)
+            let minute = (second%3600/60).toFixed(0)
+            let hour = (second/3600).toFixed(0)
+            second = second%3600%60
+            if(hour >= 1) {
+                dateFormat = `${hour}h ago`
+            } else if(minute >= 1) {
+                dateFormat = `${minute}m ago`
+            } else {
+                dateFormat = `${second}s ago`
+            }
+        }
         return dateFormat
     }
     function sortNotesByTime() {
