4eb203d230ed4d7bbaaaaf65705b6e0fb3d0fdf2
Yusheng Zhou 周宇盛 <jasonzhouu@outlook.com>
2020-03-06 08:51:42 +0800

    notes api

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require('express')
+
+const notes = require('../test/notes.js')
+
 const app = express()
 const port = 3000
 
 app.use(express.static('client'));
 
 app.get('/foo', (req, res) => res.send('bar'))
+
+
+app.get('/notes', (req, res) => res.send(notes))
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ test/notes.js
@@ -0,0 +1,20 @@
+const notes = [
+    {
+        date: '2019/03/01',
+        content: 'Bacon ipsum dolor amet beef boudin turkey cupim. Shank beef ribs pastrami pork chop chislic alcatra frankfurter picanha hamburger sirloin leberkas. Swine kevin andouille pancetta spare ribs. Ball tip turducken chislic pork belly, ground round shank kevin prosciutto pork turkey shankle jowl beef ribs. Venison cow filet mignon ball tip meatball brisket spare ribs beef shankle ground round tenderloin kielbasa pastrami bacon biltong. Beef ribs jowl t-bone, cupim shoulder bresaola chicken meatloaf sirloin.'
+    },
+    {
+        date: '2019/03/02',
+        content: 'Spare ribs leberkas pork belly meatball short loin filet mignon, bresaola pig pork chop meatloaf. Salami andouille pork loin corned beef chislic jowl turkey porchetta chicken pork shank. Tenderloin strip steak jowl, bacon pork chislic tail. Hamburger short ribs andouille jerky. Spare ribs prosciutto ball tip kevin shoulder brisket hamburger salami tenderloin chicken tongue. Spare ribs prosciutto tail sausage filet mignon. Tenderloin buffalo shankle pork ham hock venison.'
+    },
+    {
+        date: '2019/03/03',
+        content: 'Beef ribs bacon alcatra pork pastrami, chislic pancetta chicken tongue brisket. Tenderloin spare ribs short ribs drumstick landjaeger boudin meatloaf bacon pork. Pancetta short loin porchetta drumstick prosciutto. Chislic doner ground round, corned beef prosciutto tongue alcatra kevin boudin meatloaf porchetta chicken biltong filet mignon. Boudin ribeye buffalo salami bacon, brisket ham hock turducken tri-tip prosciutto chislic.'
+    },
+    {
+        date: '2019/03/04',
+        content: 'Biltong spare ribs frankfurter, landjaeger buffalo tenderloin andouille pork chop corned beef bresaola picanha turkey pork belly cow pork. Turducken cow ball tip, pig chuck flank chicken t-bone tongue bresaola corned beef landjaeger tri-tip burgdoggen pork chop. Salami short loin cow leberkas pastrami sausage. Beef chislic pastrami leberkas, drumstick tri-tip porchetta tail prosciutto capicola buffalo short ribs. Meatball fatback brisket leberkas prosciutto burgdoggen pastrami frankfurter tri-tip pork belly sausage biltong kielbasa.'
+    },
+]
+
+module.exports = notes;
