45a1959a002eaf525705df2021ed29171a8f4b68
Yusheng Zhou 周宇盛 <jasonzhouu@outlook.com>
2020-03-06 08:28:26 +0800

    hello world index.html

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('client'));
+
+app.get('/foo', (req, res) => res.send('bar'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
