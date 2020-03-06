249309a8ed571bf8ab598c18569f6141a39aeb14
Yusheng Zhou 周宇盛 <jasonzhouu@outlook.com>
2020-03-06 17:00:11 +0800

    发布note后，将新发布的note显示出来

--- client/index.js
+++ client/index.js
@@ -2,7 +2,8 @@ let notes = new Notes()
 document.querySelector('#publishNote button').addEventListener('click', event => {
     let content = document.querySelector('#noteEditor').value
     let date = Date.now()
-    // @todo: 发布note后，将新发布的note显示出来
+    // @done: 发布note后，将新发布的note显示出来：更新notes，并渲染
+    // @done: 发布note后，将输入框清空
     // @todo: 按发布时间降序排列，新发布(即最后发布)的显示在最上面
     notes.publishtNote({
         date,
@@ -11,7 +12,7 @@ document.querySelector('#publishNote button').addEventListener('click', event =>
 })
 
 function Notes() {
-    this.notes = [];
+    let notes = [];
     let getNotesUrl = "/notes"
     let postNoteUrl = "/addNotes"
     let notesList = document.querySelector('#notesList')
@@ -26,22 +27,19 @@ function Notes() {
             },
             body: JSON.stringify(note)
         }).then(response => response.json())
-        .then(data => {
-            console.log(`post note status: ${data.status}`)
-        })
+            .then(data => {
+                console.log(`post note status: ${data.status}`)
+                document.querySelector('#noteEditor').value = ""
+                notes.push(note)
+                showNotes()
+            })
     }
 
     function init() {
         getNotes(getNotesUrl)
             .then(data => {
-                // 因为箭头函数的this绑定到了init()函数作用域的this
-                // 而由于init函数的调用方式，决定了作用域的this绑定到了globalThis
-                // 所以这里的this绑定到了globalThis
-                // 解决办法1：将init()的this进行显式绑定
-                // 解决办法2：将init()改为箭头函数
-                this.notes = data;
-                let ul = renderNotes(this.notes);
-                notesList.replaceChild(ul, notesList.querySelector('ul'));
+                notes = data;
+                showNotes()
             });
     }
     function getNotes(url) {
@@ -49,7 +47,11 @@ function Notes() {
             method: 'GET'
         }).then(reponse => reponse.json())
     }
-    function renderNotes(notes) {
+    function showNotes() {
+        let ul = renderNotes();
+        notesList.replaceChild(ul, notesList.querySelector('ul'));
+    }
+    function renderNotes() {
         let ul = document.createElement('ul')
         for (const note of notes) {
             let li = document.createElement('li')
