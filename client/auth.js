eacc348a5871257411f8e9d3589e95f40e01a5e7
Yusheng Zhou 周宇盛 <jasonzhouu@outlook.com>
2020-03-08 13:13:45 +0800

    hashpassword in client

--- client/auth.js
+++ client/auth.js
@@ -7,24 +7,26 @@ if (loginStatus == 'true') {
 
 
 function login(username, password) {
-    fetch('/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            username,
-            password
-        })
-    }).then(reponse => reponse.json())
-        .then(data => {
-            let { status } = data
-            console.log(status)
-            if (status == 'login sucessfully') {
-                localStorage.setItem('login', 'true')
-                show()
-            }
-        })
+    hash(password).then(passwordHash => {
+        fetch('/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username,
+                passwordHash
+            })
+        }).then(reponse => reponse.json())
+            .then(data => {
+                let { status } = data
+                console.log(status)
+                if (status == 'login sucessfully') {
+                    localStorage.setItem('login', 'true')
+                    show()
+                }
+            })
+    })
 }
 
 function logout() {
@@ -44,4 +46,12 @@ function hide() {
     document.querySelectorAll('#notesList > ul > li > button').forEach(element => {
         element.style.display = 'none'
     })
+}
+
+async function hash(message) {
+    const msgUint8 = new TextEncoder().encode(message);                           // encode as (utf-8) Uint8Array
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);           // hash the message
+    const hashArray = Array.from(new Uint8Array(hashBuffer));                     // convert buffer to byte array
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join(''); // convert bytes to hex string
+    return hashHex;
 }
